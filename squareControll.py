1b87b0254055c6efb2522c88e13c00741ce77ccd
TopoPhillMan <88756720+TopoPhillMan@users.noreply.github.com>
2022-03-27 15:16:44 -0700

    Changes 3/27/2022 3:16
    Added ChangeSquare

--- squareControll.py
+++ squareControll.py
@@ -9,11 +9,27 @@ class rowX:
 class board:
     x = []
 
-def makeBoard(xHight,yHight):
-    mainBoard = board()
-    for x in range(xHight):
-        board.x.append(rowX)
-        for a in range(yHight):
-            board.x[0].y.append(boardSquare)
+mainBoard = board()
 
-makeBoard(3,3)
+def makeBoard():
+    for a in range(3):
+        mainBoard.x.append(rowX)
+        for b in range(3):
+            mainBoard.x[a].y.append("-")
+
+def changeSqare(x,y,changeTo):
+    if x == "a":
+        x = 0
+    elif x == "b":
+        x = 1
+    elif x == "c":
+        x = 2
+    else: 
+        print("ERROR: Unknown x value")
+        
+    mainBoard.x[x].y[y] = changeTo
+
+makeBoard()
+changeSqare("a",0,"x")
+print(mainBoard.x[0].y[0])
+
