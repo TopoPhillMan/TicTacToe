50787fa6cc519bcfd933458d29de2364d962dae5
TopoPhillMan <88756720+TopoPhillMan@users.noreply.github.com>
2022-03-27 16:22:16 -0700

    Changes 4:22 3/27/2022

--- squareControll.py
+++ squareControll.py
@@ -1,35 +1,36 @@
-
-
-class boardSquare:
-    value = "-"
-
-class rowX:
-    y = []
+class row:
+    y0 = ""
+    y1 = ""
+    y2 = ""
 
 class board:
-    x = []
+    x = [row,row,row]
+    
+    def __getitem__(self, i):
+        return f"Value {i}"
 
 mainBoard = board()
 
-def makeBoard():
-    for a in range(3):
-        mainBoard.x.append(rowX)
-        for b in range(3):
-            mainBoard.x[a].y.append("-")
-
 def changeSqare(x,y,changeTo):
     if x == "a":
-        x = 0
+        x = "0"
     elif x == "b":
-        x = 1
+        x = "1"
     elif x == "c":
-        x = 2
+        x = "2"
     else: 
         print("ERROR: Unknown x value")
         
-    mainBoard.x[x].y[y] = changeTo
+    mainBoard.x[x[y]] = changeTo
 
-makeBoard()
-changeSqare("a",0,"x")
-print(mainBoard.x[0].y[0])
+def printBoard():
+    print(f"{mainBoard.x[0].y0} | {mainBoard.x[1].y0} | {mainBoard[2].y0}")
+    print("──┼──┼──")
+    print(f"{mainBoard.x[0].y1} | {mainBoard.x[1].y1} | {mainBoard[2].y1}")
+    print("──┼──┼──")
+    print(f"{mainBoard.x[0].y2} | {mainBoard.x[1].y2} | {mainBoard[2].y2}")
+
+
+printBoard()
+
 
