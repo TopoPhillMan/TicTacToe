94264f0b1783a7fa5843fdbb8c1d7d53d4d70250
TopoPhillMan <88756720+TopoPhillMan@users.noreply.github.com>
2022-07-13 01:10:46 -0400

    Finish Map Gen - Test Code Biuld Out - Map Operators in Place

--- golfFunctions.py
+++ golfFunctions.py
@@ -1,3 +1,5 @@
+from functools import cache
+from random import random, randrange
 import squareControl as sC
 import gameControl as gC
 import ticFunctions as tic
@@ -12,17 +14,83 @@ class ball():
     weight = int # Kilos
 
 mainBall = ball()
+mapBoard = sC.board()
 
-def calcAirTime(yInitial, velocityInitial, gravity, angle):
-    # yInital = velocityY*time + (0.5*gravity)*time**2
+def changeAlt(startX,startY,altGain):
+    for x in range(abs(altGain)):
+        if altGain > 0: 
+            golf.mapBoard.x[startX+x][startY+x] = "/"
+        if altGain == 0:
+            golf.mapBoard.x[startX+x][startY] = "_"
+        if altGain < 0:
+            golf.mapBoard.x[startX+x][startY-(x+1)] = f"\\"
+
+def addStraight(startX,startY,length):
+    for x in range(length):
+        golf.mapBoard.x[startX+x][startY] = "_"
+
+def genMap():
+    startAlt = int
+
+    startAlt = round(0.25*golf.mapBoard.yRows)
+    operatingCache = int
+    operatingPosX = 3
+    operatingPosY = startAlt
+    operatingContinue = True
+
+
+    golf.mapBoard.x[0][startAlt] = "_"
+    golf.mapBoard.x[1][startAlt] = "▂"
+    golf.mapBoard.x[2][startAlt] = "_"
+
+    while operatingContinue:
+        if operatingPosX >= 0.50*golf.mapBoard.xRows:
+            operatingContinue = False
+            continue
+
+        operatingCache = round(((golf.mapBoard.xRows - 3)*0.4)*random())
+        golf.addStraight(operatingPosX, operatingPosY, operatingCache)
+        operatingPosX += operatingCache
+
+        operatingCache = random()
+        if operatingCache <= 0.25:
+            operatingCache = -2
+        elif operatingCache > 0.25 and operatingCache <= 0.5:
+            operatingCache = -1
+        elif operatingCache > 0.5 and operatingCache <= 0.75:
+            operatingCache = 1
+        elif operatingCache > 0.75 and operatingCache <= 1:
+            operatingCache = 2
+
+        if operatingPosY + operatingCache < 1:
+            operatingCache = abs(operatingCache)
+
+        golf.changeAlt(operatingPosX,operatingPosY,operatingCache)
+        operatingPosX += (abs(operatingCache))
+        operatingPosY += operatingCache
+
+    operatingCache = golf.mapBoard.xRows - operatingPosX
+    golf.addStraight(operatingPosX, operatingPosY, operatingCache)
+
+    operatingContinue = golf.mapBoard.xRows - round(operatingCache*random())
+    golf.mapBoard.x[operatingContinue][operatingPosY] = " "
+    golf.mapBoard.x[operatingContinue+1][operatingPosY] = " "
+    golf.mapBoard.x[operatingContinue][operatingPosY-1] = "└"
+    golf.mapBoard.x[operatingContinue+1][operatingPosY-1] = "┘"
+
+
+
+def calcAirTime(yChange, velocityInitial, gravity, angle):
+    # y = yInital + velocityY*time + (0.5*gravity)*time**2
     # time = (-1*(velocityY) +- sqrt(velocityY**2 -4*(0.5*gravity)*yInitial)) / 0.5*gravity
 
     velocityY = velocityInitial*math.sin(math.radians(angle))
-    squareRootValue = velocityY**2 -4*(0.5*gravity)*yInitial
-    if yInitial >= 0:
+    squareRootValue = velocityY**2 -4*(0.5*gravity)*yChange
+    if yChange >= 0:
         time = (-1*velocityY - math.sqrt(squareRootValue)) / 0.5*gravity
-    elif yInitial < 0:
-        time = (-1*(velocityY) + math.sqrt(velocityY**2 -4*(0.5*gravity)*yInitial)) / 0.5*gravity
+    elif yChange < 0:
+        time = (-1*(velocityY) + math.sqrt(velocityY**2 -4*(0.5*gravity)*yChange)) / 0.5*gravity
     
     return time
+
 
--- testing.py
+++ testing.py
@@ -1,3 +1,4 @@
+from random import random
 import gameControl as gC
 import squareControl as sC
 import ticFunctions as tic
@@ -5,8 +6,8 @@ import connectFunctions as con
 import golfFunctions as golf
 import math
 
-sC.mainBoard.xRows = 6
-sC.mainBoard.yRows = 3
+sC.mainBoard.xRows = 40
+sC.mainBoard.yRows = 10
 sC.mainBoard.x = sC.board.genBoard(sC.mainBoard.xRows, sC.mainBoard.yRows)
 
 golf.mainBall.x = 1
@@ -14,4 +15,15 @@ golf.mainBall.y = 3
 golf.mainBall.sprite = "B"
 golf.mainBall.weight = 1
 
-print(golf.calcAirTime(3, 20, -9.8, 20))
+golf.mapBoard.xRows = sC.mainBoard.xRows
+golf.mapBoard.yRows = sC.mainBoard.yRows
+golf.mapBoard.x = sC.board.genBoard(golf.mapBoard.xRows, golf.mapBoard.yRows)
+
+# golf.changeAlt(0,1,2)
+# golf.addStraight(2,3,3)
+# golf.changeAlt(5,3,-2)
+
+golf.genMap()
+
+sC.mainBoard.x = golf.mapBoard.x
+sC.printBoardCombined(sC.mainBoard.yRows,sC.mainBoard.xRows)
