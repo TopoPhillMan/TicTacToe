60f159e6b33748c9c36d426f154932075085f598
TopoPhillMan <88756720+TopoPhillMan@users.noreply.github.com>
2022-11-01 23:45:04 -0700

    Wall Creation Works

--- ghostFunctions.py
+++ ghostFunctions.py
@@ -2,6 +2,7 @@ import gameControl as gC
 import squareControl as sC
 import ghostFunctions as gho
 from msvcrt import getch
+from random import random
 
 # Layer entrance - ⛶
 # Player character - ⚇
@@ -25,8 +26,9 @@ def printBoard(xLines, yLines):
     print(outputString)
 
 def biuldMap(xLines, yLines):
-    gho.mazeMap = sC.board.genBoard(xLines,yLines)
-    gho.charcterMap = sC.board.genBoard(xLines,yLines)
+    gho.mazeMap = sC.board.genBoard(xLines*3,yLines*3)
+    gho.charcterMap = sC.board.genBoard(xLines*3,yLines*3)
+    gho.wallMap = mazeStackCell.genBoard(xLines,yLines)
 
 
 class cluster():
@@ -59,7 +61,121 @@ def addCluster(cluster,startX,startY):
 
     for y in range(yLength):
         for x in range(xLength):
-            gho.mazeMap[startY+y][startX+x] = cluster[y][x]
+            gho.charcterMap[startY+y][startX+x] = cluster[y][x]
+
+class mazeStackCell():
+    def __init__(self, visited, x, y):
+        self.self = self
+        self.visited = visited
+        self.x = x 
+        self.y = y
+    north = True
+    south = True 
+    east = True
+    west = True
+
+    def genBoard(xLine, yLine):
+        boardTemp = []
+        for x in range(xLine):
+            boardTemp.insert(xLine, [])
+    
+        for x in range(xLine):
+            for y in range(yLine):
+                boardTemp[x].append(mazeStackCell(False,x,y))
+        return boardTemp
+    
+    def checkDeadEnd(self):
+        xLine = self.x
+        yLine = self.y
+
+        tracker = 0
+        if yLine - 1 >= 0:
+            if wallMap[xLine][yLine-1].visited == True:
+                tracker += 1
+        else: 
+            tracker += 1
+
+        if yLine + 1 <= len(gho.wallMap[0]):
+            if wallMap[xLine][yLine+1].visited == True:
+                tracker += 1
+        else: 
+            tracker += 1
+        
+        if xLine +1 <= len(gho.wallMap):
+            if wallMap[xLine+1][yLine].visited == True:
+                tracker += 1
+        else: 
+            tracker += 1
+
+        if xLine -1 >= 0:
+            if wallMap[xLine-1][yLine].visited == True:
+                tracker += 1
+        else:
+            tracker += 1
+        
+        if tracker == 4:
+            return True
+        else:
+            return False
+        
+    def chooseJunction(self):
+
+        xLine = self.x
+        yLine = self.y
+        while True:
+            ran = random()
+            if ran >= 0 and ran < 0.25 and yLine - 1 >= 0:
+                if wallMap[xLine-0][yLine-1].visited == False:
+                    return 'north'
+            elif ran >= 0.25 and ran < 0.5 and yLine + 1 <= len(gho.wallMap[0]):
+                if wallMap[xLine][yLine+1].visited == False:
+                    return 'south'
+            elif ran >= 0.5 and ran < 0.75 and xLine +1 <= len(gho.wallMap):
+                if wallMap[xLine+1][yLine].visited == False:
+                    return 'east'
+            elif ran >= 0.75 and ran <= 1 and xLine -1 >= 0:
+                if wallMap[xLine-1][yLine].visited == False:
+                    return 'west'
+            
+    def addToPath(x,y):
+        mainStackPath.append((x,y))
+    
+    def backTrack():
+        del mainStackPath[-1]
+    
+    def goDirection(self,direction):
+        if direction == 'north':
+            self.north = False
+            return (self.x,self.y-1)
+        elif direction == 'south':
+            self.south = False
+            return (self.x,self.y+1)
+        elif direction == 'east':
+            self.east = False
+            return (self.x+1,self.y)
+        elif direction == 'west':
+            self.west = False
+            return (self.x-1,self.y)
+    
+    def makeMaze(self):
+        mazeStackCell.addToPath(0,0)
+        while (wallMap[0][0].checkDeadEnd() != True):
+            currentX = mainStackPath[-1][0]
+            currentY = mainStackPath[-1][1]
+
+            if wallMap[currentX][currentY].checkDeadEnd():
+                mazeStackCell.backTrack()
+                continue
+            else:
+                cords = wallMap[currentX][currentY].goDirection(wallMap[currentX][currentY].chooseJunction())
+                mazeStackCell.addToPath(cords[0], cords[1])
 
 
- 
+
+
+wallMap = gho.mazeStackCell
+mainStackPath = []
+
+
+
+
--- testing.py
+++ testing.py
@@ -13,16 +13,22 @@ from msvcrt import getch
 # ═ ║ ╒ ╓ ╔ ╕ ╖ ╗ ╘ ╙ ╚ ╛ ╜ ╝ ╞ ╟ 
 # ╠ ╡ ╢ ╣ ╤ ╥ ╦ ╧ ╨ ╩ ╪ ╫ ╬ 
 
-sC.mainBoard.x = sC.board.genBoard(10,10)
-borderCluser = [["┏","━","━","━","━","━","━","━","━","┓"],["┃"," "," "," "," "," "," "," "," ","┃"],["┃"," "," "," "," "," "," "," "," ","┃"],["┃"," "," "," "," "," "," "," "," ","┃"],["┃"," "," "," "," "," "," "," "," ","┃"],["┃"," "," "," "," "," "," "," "," ","┃"],["┃"," "," "," "," "," "," "," "," ","┃"],["┃"," "," "," "," "," "," "," "," ","┃"],["┃"," "," "," "," "," "," "," "," ","┃"],["┗","━","━","━","━","━","━","━","━","┛"]]
-testCluster = [["h","e","l","l","o"],["w","o","r","l","d"]]
+# sC.mainBoard.x = sC.board.genBoard(10,10)
+# borderCluser = [["┏","━","━","━","━","━","━","━","━","┓"],["┃"," "," "," "," "," "," "," "," ","┃"],["┃"," "," "," "," "," "," "," "," ","┃"],["┃"," "," "," "," "," "," "," "," ","┃"],["┃"," "," "," "," "," "," "," "," ","┃"],["┃"," "," "," "," "," "," "," "," ","┃"],["┃"," "," "," "," "," "," "," "," ","┃"],["┃"," "," "," "," "," "," "," "," ","┃"],["┃"," "," "," "," "," "," "," "," ","┃"],["┗","━","━","━","━","━","━","━","━","┛"]]
+# testCluster = [["h","e","l","l","o"],["w","o","r","l","d"]]
 gho.biuldMap(10,10)
-gho.addCluster(testCluster,1,2)
-gho.addCluster(gho.cluster.straight.vertical,1,4)
-gho.addCluster(gho.cluster.straight.vertical,1,5)
-gho.addCluster(gho.cluster.straight.vertical,1,6)
-gho.addCluster(borderCluser,0,0)
-sC.mainBoard.x = gho.mazeMap
-gho.printBoard(10,10)
+# gho.addCluster(testCluster,1,2)
+# gho.addCluster(gho.cluster.straight.vertical,1,4)
+# gho.addCluster(gho.cluster.straight.vertical,1,5)
+# gho.addCluster(gho.cluster.straight.vertical,1,6)
+# # gho.addCluster(borderCluser,0,0)
+# sC.mainBoard.x = gho.charcterMap
+# gho.printBoard(10,10)
 
+gho.wallMap[0][0].visited = True
+gho.wallMap[1][0].visited = True
+
+
+gho.wallMap[0][0].makeMaze()
+print('a')
 
